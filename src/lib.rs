23ee0ef7323ab9f082c51d7b02eb9b84893c5cae
MizurenNanako <ceoigingtong@outlook.com>
2025-06-12 19:20:45 +0800

    finished

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, iter::repeat_with};
+use std::fmt::{self, Display};
 
 /// Binary S-Expression
 /// - Atoms are in bytes
@@ -11,8 +11,8 @@ pub enum BSExp {
 
 impl BSExp {
     /// Create a new BSExp from an atom
-    pub fn atom<T: Into<Vec<u8>>>(value: T) -> Self {
-        BSExp::Atom(value.into())
+    pub fn atom<T: Into<BSExp>>(value: T) -> Self {
+        value.into()
     }
 
     /// Create a new BSExp from a list of BSExp
@@ -21,234 +21,131 @@ impl BSExp {
     }
 }
 
-impl std::fmt::Display for BSExp {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            BSExp::Atom(a) => write!(f, "{}", String::from_utf8_lossy(a)),
-            BSExp::List(l) => {
-                let mut iter = l.iter();
-                write!(f, "(")?;
-                if let Some(first) = iter.next() {
-                    write!(f, "{}", first)?;
-                    for item in iter {
-                        write!(f, " {}", item)?;
-                    }
-                }
-                write!(f, ")")
-            }
-        }
+impl From<&str> for BSExp {
+    fn from(s: &str) -> Self {
+        BSExp::Atom(s.as_bytes().to_vec())
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_atom() {
-        let atom = BSExp::atom("hello");
-        assert_eq!(format!("{}", atom), "hello");
-    }
-
-    #[test]
-    fn test_list() {
-        let list = BSExp::list([BSExp::atom("a"), BSExp::atom("b")]);
-        assert_eq!(format!("{}", list), "(a b)");
-    }
-
-    #[test]
-    fn test_nested_list() {
-        let nested = BSExp::list([
-            BSExp::atom("a"),
-            BSExp::list([BSExp::atom("b"), BSExp::atom("c")]),
-        ]);
-        assert_eq!(format!("{}", nested), "(a (b c))");
+impl From<String> for BSExp {
+    fn from(s: String) -> Self {
+        BSExp::Atom(s.into_bytes())
     }
 }
 
-// Idx is used to represent indices in the constant and node pools.
-// It is a signed 32-bit integer, allowing for a range of -2,147,483,648 to 2,147,483,647.
-// For node pools we use positive indices for nodes,
-//   and negative indices are for atoms.
-// For constant pool, positive indices are for constants,
-//   and negative indices for special constants.
+#[macro_export]
+macro_rules! bsexp {
+    // Match a list expression: [ ... ]
+    ( [ $( $elem:tt ),* ] ) => {
+        $crate::BSExp::list(vec![
+            $( bsexp!($elem) ),*
+        ])
+    };
 
-type Idx = i64;
-impl BSExp {
-    /// Serialize the BSExp into a byte array.
-    pub fn to_bytes(&self) -> Vec<u8> {
-        let mut symbol_table = HashMap::new();
-        let mut constant_pool = Vec::<u8>::new();
-        let mut node_pool = Vec::new();
-        self.serialize(&mut symbol_table, &mut constant_pool, &mut node_pool);
-
-        // The first is the length of the constant pool (Idx)
-        // The second is the length of the node pool (Idx)
-        // The rest is the constant pool (byte array) followed by the node pool (Idx array)
-        let mut result = Vec::new();
-        result.extend((constant_pool.len() as Idx).to_le_bytes());
-        result.extend((node_pool.len() as Idx).to_le_bytes());
-        result.extend(constant_pool);
-        result.extend(node_pool.into_iter().map(|idx| idx.to_le_bytes()).flatten());
-        result
-    }
-
-    fn serialize(
-        &self,
-        symbol_table: &mut HashMap<Vec<u8>, Idx>,
-        constant_pool: &mut Vec<u8>,
-        node_pool: &mut Vec<Idx>,
-    ) {
-        match self {
-            BSExp::Atom(a) => {
-                if let Some(&index) = symbol_table.get(a) {
-                    // We have seen this atom before, use its index
-                    // Negative indices are used for atoms in the node pool
-                    node_pool.push(!index);
-                } else {
-                    // This is a new atom, add it to the constant pool
-                    let index = constant_pool.len() as Idx;
-                    constant_pool.extend_from_slice((a.len() as Idx).to_le_bytes().as_ref());
-                    constant_pool.extend_from_slice(a);
-                    symbol_table.insert(a.clone(), index);
-                    node_pool.push(!index);
-                }
-            }
-            BSExp::List(l) => {
-                // list head: a length
-                node_pool.push(l.len() as Idx);
-                for item in l {
-                    item.serialize(symbol_table, constant_pool, node_pool);
-                }
-            }
-        }
-    }
+    // Match a single atom expression
+    ( $atom:expr ) => {
+        $crate::BSExp::atom($atom)
+    };
 }
 
-fn read_idx<'a, I: Iterator<Item = &'a u8>>(iter: &mut I) -> Result<Idx, String> {
-    let mut bytes = [0u8; size_of::<Idx>()];
-    for i in 0..4 {
-        if let Some(&byte) = iter.next() {
-            bytes[i] = byte;
+impl Display for BSExp {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if f.alternate() {
+            self.fmt_pretty(f, 0)
         } else {
-            return Err("Not enough bytes to read.".to_string());
-        }
-    }
-    Ok(Idx::from_le_bytes(bytes))
-}
-
-impl BSExp {
-    /// Deserialize a byte array into a BSExp.
-    pub fn from_bytes(bytes: &[u8]) -> Result<Self, String> {
-        let mut cursor = bytes.iter();
-        let constant_pool_length = read_idx(&mut cursor)? as usize;
-        let node_pool_length = read_idx(&mut cursor)? as usize;
-
-        // Read the constant pool
-        let constant_pool = cursor
-            .by_ref()
-            .take(constant_pool_length)
-            .cloned()
-            .collect::<Vec<u8>>();
-
-        let mut idx_cursor = repeat_with(|| read_idx(&mut cursor)).take(node_pool_length as usize);
-        Self::deserialize(&mut idx_cursor, &constant_pool)
-    }
-
-    fn deserialize<'a, I: Iterator<Item = Result<Idx, String>>>(
-        idx_cursor: &mut I,
-        constant_pool: &Vec<u8>,
-    ) -> Result<Self, String> {
-        match idx_cursor.next() {
-            // This is an empty list
-            Some(Ok(0)) => Ok(BSExp::List(Vec::new())),
-            // This is an atom
-            Some(Ok(idx)) if idx < 0 => {
-                let idx = !idx as usize;
-                let len: &[u8; size_of::<Idx>()] = &constant_pool[idx..idx + size_of::<Idx>()]
-                    .try_into()
-                    .map_err(|_| "Invalid constant pool index.".to_string())?;
-                let idx = idx + 4;
-                let len = Idx::from_le_bytes(*len);
-                Ok(BSExp::Atom(constant_pool[idx..idx + len as usize].to_vec()))
+            match self {
+                BSExp::Atom(item) => match String::from_utf8(item.clone()) {
+                    Ok(s) => f.write_str(s.as_str()),
+                    Err(_) => write!(
+                        f,
+                        "{}",
+                        item.iter()
+                            .map(|b| b.to_string())
+                            .collect::<Vec<_>>()
+                            .join(" ")
+                    ),
+                },
+                BSExp::List(items) => write!(
+                    f,
+                    "({})",
+                    items
+                        .iter()
+                        .map(|x| x.to_string())
+                        .collect::<Vec<_>>()
+                        .join(" ")
+                ),
             }
-            // This is a non-empty list
-            Some(Ok(idx)) => Ok(BSExp::List(
-                repeat_with(|| Self::deserialize(idx_cursor, constant_pool))
-                    .take(idx as usize)
-                    .collect::<Result<Vec<_>, _>>()?,
-            )),
-            _ => Err("Invalid index encountered during deserialization.".to_string()),
+        }
+    }
+}
+impl BSExp {
+    fn indention(f: &mut fmt::Formatter<'_>, indent: usize) -> Result<(), std::fmt::Error> {
+        (0..indent).map(|_| f.write_str(" ")).collect()
+    }
+
+    fn fmt_pretty(&self, f: &mut fmt::Formatter<'_>, indent: usize) -> fmt::Result {
+        match self {
+            BSExp::Atom(_) => {
+                BSExp::indention(f, indent)?;
+                write!(f, "{self}")
+            }
+
+            BSExp::List(v) => {
+                let mut buf = String::new();
+                use std::fmt::Write as _;
+                write!(&mut buf, "{self}")?;
+
+                if buf.len() < f.width().unwrap_or(60) {
+                    BSExp::indention(f, indent)?;
+                    f.write_str(&buf)
+                } else {
+                    BSExp::indention(f, indent)?;
+                    f.write_str("(")?;
+
+                    let mut it = v.iter();
+                    if let Some(x) = it.next() {
+                        match x {
+                            BSExp::Atom(_) => write!(f, "{x}")?,
+                            _ => {
+                                f.write_str("\n")?;
+                                x.fmt_pretty(f, indent + 1)?
+                            }
+                        }
+                    }
+
+                    for x in it {
+                        f.write_str("\n")?;
+                        x.fmt_pretty(f, indent + 1)?;
+                    }
+
+                    f.write_str(")")
+                }
+            }
         }
     }
 }
 
-#[cfg(test)]
-mod serialization_tests {
-    use super::*;
-
-    #[test]
-    fn test_empty_list() {
-        let exp = BSExp::list([]);
-        let bytes = exp.to_bytes();
-        println!("Serialized bytes: {:?}", bytes);
-        let deserialized = BSExp::from_bytes(&bytes).unwrap();
-        assert_eq!(format!("{}", deserialized), "()");
-    }
-
-    #[test]
-    fn test_single_atom() {
-        let exp = BSExp::atom("test");
-        let bytes = exp.to_bytes();
-        println!("Serialized bytes: {:?}", bytes);
-        let deserialized = BSExp::from_bytes(&bytes).unwrap();
-        assert_eq!(format!("{}", deserialized), "test");
-    }
-
-    #[test]
-    fn test_nested_list() {
-        let exp = BSExp::list([
-            BSExp::atom("a"),
-            BSExp::list([BSExp::atom("b"), BSExp::atom("c")]),
-        ]);
-        let bytes = exp.to_bytes();
-        println!("Serialized bytes: {:?}", bytes);
-        let deserialized = BSExp::from_bytes(&bytes).unwrap();
-        assert_eq!(format!("{}", deserialized), "(a (b c))");
-    }
-
-    #[test]
-    fn test_complex_structure() {
-        let exp = BSExp::list([
-            BSExp::atom("x"),
-            BSExp::list([BSExp::atom("y"), BSExp::atom("z")]),
-            BSExp::atom("w"),
-        ]);
-        let bytes = exp.to_bytes();
-        println!("Serialized bytes: {:?}", bytes);
-        let deserialized = BSExp::from_bytes(&bytes).unwrap();
-        assert_eq!(format!("{}", deserialized), "(x (y z) w)");
-    }
-
-    #[test]
-    fn test_atom_reuse() {
-        let exp1 = BSExp::atom("shared");
-        let exp2 = BSExp::list([exp1.clone(), exp1]);
-        let bytes = exp2.to_bytes();
-        println!("Serialized bytes: {:?}", bytes);
-        let deserialized = BSExp::from_bytes(&bytes).unwrap();
-        assert_eq!(format!("{}", deserialized), "(shared shared)");
-    }
-
-    #[test]
-    fn test_complex_reuse() {
-        let exp1 = BSExp::atom("a");
-        let exp2 = BSExp::atom("b");
-        let exp3 = BSExp::list([exp1.clone(), exp2.clone()]);
-        let exp4 = BSExp::list([exp3.clone(), exp1, exp2]);
-        let bytes = exp4.to_bytes();
-        println!("Serialized bytes: {:?}", bytes);
-        let deserialized = BSExp::from_bytes(&bytes).unwrap();
-        assert_eq!(format!("{}", deserialized), "((a b) a b)");
-    }
+#[test]
+fn test_bsexp_format() {
+    let fib = bsexp!([
+        "define",
+        ["fibonacci", "n"],
+        [
+            "define",
+            ["fib-iter", "a", "b", "count"],
+            [
+                "if",
+                ["=", "count", "0"],
+                "a",
+                ["fib-iter", "b", ["+", "a", "b"], ["-", "count", "1"]]
+            ]
+        ],
+        ["fib-iter", "0", "1", "n"]
+    ]);
+    assert_eq!(
+        format!("{fib:#}"),
+        "(define\n (fibonacci n)\n (define\n  (fib-iter a b count)\n  (if (= count 0) a (fib-iter b (+ a b) (- count 1))))\n (fib-iter 0 1 n))"
+    );
 }
+
+pub mod vli;
---
+++ src/vli.rs
@@ -0,0 +1,124 @@
+use std::iter::repeat_with;
+
+/// Variant-Length Integer
+/// This type can be serialized to 1 to 8 bytes
+/// encoding atmost 2^56 bit data
+pub trait VLI {
+    fn write_vli_bytes<F, E>(self, writer: F) -> Result<usize, E>
+    where
+        F: FnMut(u8) -> Result<(), E>;
+
+    fn read_vli_bytes<F, E>(reader: F) -> Result<Self, E>
+    where
+        Self: Sized,
+        F: FnMut() -> Result<u8, E>;
+}
+
+const MAX_ENCODING_LENGTH: usize = 9;
+
+impl VLI for u64 {
+    #[inline]
+    fn write_vli_bytes<F, E>(self, mut writer: F) -> Result<usize, E>
+    where
+        F: FnMut(u8) -> Result<(), E>,
+    {
+        let tmp = self as u64;
+        assert!((tmp & (0b1 << 63) == 0));
+
+        // segments:
+        // 7 bits + (7 bits + 7 bits + 7 bits + 7 bits + 7 bits + 7 bits + 7 bits)
+        let segs = [
+            (tmp & 0b0111_1111) as u8,
+            ((tmp & (0b0111_1111 << (1 * 7))) >> (1 * 7)) as u8,
+            ((tmp & (0b0111_1111 << (2 * 7))) >> (2 * 7)) as u8,
+            ((tmp & (0b0111_1111 << (3 * 7))) >> (3 * 7)) as u8,
+            ((tmp & (0b0111_1111 << (4 * 7))) >> (4 * 7)) as u8,
+            ((tmp & (0b0111_1111 << (5 * 7))) >> (5 * 7)) as u8,
+            ((tmp & (0b0111_1111 << (6 * 7))) >> (6 * 7)) as u8,
+            ((tmp & (0b0111_1111 << (7 * 7))) >> (7 * 7)) as u8,
+            ((tmp & (0b0111_1111 << (8 * 7))) >> (8 * 7)) as u8,
+        ];
+
+        let len = MAX_ENCODING_LENGTH - segs.iter().cloned().rev().take_while(|&x| x == 0).count();
+        let mut it = segs.into_iter();
+        (&mut it)
+            .take(len - 1)
+            .try_for_each(|x| writer(x | 0b1000_0000))
+            .map(|()| len)?;
+        it.take(1).try_for_each(|x| writer(x)).map(|()| len)
+    }
+
+    #[inline]
+    fn read_vli_bytes<F, E>(mut reader: F) -> Result<Self, E>
+    where
+        F: FnMut() -> Result<u8, E>,
+    {
+        repeat_with(|| reader())
+            .take(MAX_ENCODING_LENGTH)
+            .try_fold(0, |acc, x| match x {
+                Ok(x) => {
+                    let q = (acc << 7) + (x & 0b0111_1111) as u64;
+                    if (x & 0b1000_0000) != 0 {
+                        Ok(q)
+                    } else {
+                        Err((q, None))
+                    }
+                }
+                Err(e) => Err((0, Some(e))),
+            })
+            .err()
+            .map(|(r, e)| match e {
+                Some(e) => Err(e),
+                None => Ok(r),
+            })
+            .unwrap() // can always unwrap
+    }
+}
+
+#[test]
+fn test_vli_encode_decode() {
+    use core::convert::Infallible;
+    use std::iter::repeat_with;
+    type I = Result<(), Infallible>;
+    use VLI;
+
+    // test writing
+    let mut buf = Vec::new();
+    let v1 = [
+        127,
+        16383,
+        2097151,
+        268435455,
+        34359738367,
+        4398046511103,
+        562949953421311,
+        72057594037927935,
+        9223372036854775807,
+        // 18446744073709551615,
+    ];
+    v1.iter().for_each(|i| {
+        i.write_vli_bytes(|x| I::Ok(buf.push(x))).unwrap();
+    });
+
+    // test reading
+    let mut cursor = buf.into_iter();
+
+    assert_eq!(
+        repeat_with(|| {
+            <u64 as VLI>::read_vli_bytes(|| match cursor.next() {
+                Some(x) => Ok(x),
+                None => Err(()),
+            })
+        })
+        .map_while(|x| match x {
+            Ok(x) => Some(x),
+            Err(_) => None,
+        })
+        .zip(v1.into_iter())
+        .fold(true, |acc, (l, r)| {
+            println!("{l:064b} {r:064b}");
+            acc && (l == r)
+        }),
+        true
+    )
+}
